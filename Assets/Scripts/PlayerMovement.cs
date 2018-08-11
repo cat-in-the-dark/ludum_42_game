cd7e9c03305af86a6fc5f658d269700b547b761b
Ilya Siganov <ilya.blan4@gmail.com>
2018-08-11 18:39:04 +0600

    add run flip, background

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,20 +7,22 @@ public class PlayerMovement : MonoBehaviour
 
     private BoxCollider2D collider;
     private Rigidbody2D body;
-    private float inverseMoveTime;
+    private Animator animator;
+    private SpriteRenderer renderer;
 
     private const float EPSILON = 0.001f;
     private float sqrRemainingDistance = 0f;
     private Vector3 end = Vector3.zero;
     private bool isMoving = false;
-    
+    private bool flipX = false;
 
     // Use this for initialization
     void Start()
     {
+        animator = GetComponent<Animator>();
         collider = GetComponent<BoxCollider2D>();
         body = GetComponent<Rigidbody2D>();
-        inverseMoveTime = 1f / moveTime;
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -29,17 +30,19 @@ public class PlayerMovement : MonoBehaviour
     {
         SmoothMovement();
 
-        int h = (int)Input.GetAxisRaw("Horizontal");
-        int v = (int)Input.GetAxisRaw("Vertical");
+        int h = (int) Input.GetAxisRaw("Horizontal");
+        int v = (int) Input.GetAxisRaw("Vertical");
 
         if (h == 0) return;
-        
+
         if (h < 0)
         {
+            flipX = true;
             TryMove(Vector3.left);
         }
         else
         {
+            flipX = false;
             TryMove(Vector3.right);
         }
     }
@@ -47,25 +50,29 @@ public class PlayerMovement : MonoBehaviour
     bool TryMove(Vector3 dir)
     {
         if (isMoving) return false;
-        
+        renderer.flipX = flipX;
+
         var start = transform.position;
         end = start + dir;
-        
+
         collider.enabled = false;
         var hit = Physics2D.Linecast(start, end, blockingLayer);
         collider.enabled = true;
-        
+
         if (hit.transform != null) return false;
-        
+
         Debug.LogFormat("MOVE {0} to {1}", start, end);
         isMoving = true;
-        
+        OnStartMoving();
+
         return true;
     }
 
     private void SmoothMovement()
     {
+        // TODO: CHECK THAT OBJECT DOESN't STUCK in box collider
         if (!isMoving) return;
+        var inverseMoveTime = 1f / moveTime;
         sqrRemainingDistance = (transform.position - end).sqrMagnitude;
         if (sqrRemainingDistance > EPSILON)
         {
@@ -76,6 +83,19 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             isMoving = false;
+            OnStopMoving();
         }
+    }
+
+    private void OnStartMoving()
+    {
+        // TODO: it should be kind of event?
+        animator.SetBool("playerRunning", true);
+    }
+
+    private void OnStopMoving()
+    {
+        // TODO: it should be kind of event?
+        animator.SetBool("playerRunning", false);
     }
 }
