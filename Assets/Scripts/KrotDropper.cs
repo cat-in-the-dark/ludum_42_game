aa484a02ba4f08efdbff6fc28c30e3db8ffc581e
Ilya Siganov <ilya.blan4@gmail.com>
2018-08-12 16:20:22 +0600

    add textures, add krot

--- Assets/Scripts/KrotDropper.cs
+++ Assets/Scripts/KrotDropper.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 
-public class KrotDropper : MonoBehaviour {
-
+public class KrotDropper : MonoBehaviour
+{
+	public GameObject Krot;
+	
 	// Use this for initialization
 	void Start () {
-		
+		Invoke("Drop", 4f);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Drop()
+	{
+		var inst = Instantiate(Krot, new Vector3(-1.5f, -1.5f, 0f), Quaternion.identity);
+		Destroy(inst, 7f);
 	}
 }
